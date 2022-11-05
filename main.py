579a00f467706cae07e69bfe7a935570811c664a
Ernest Morse <enmorse48@gmail.com>
2022-11-05 02:10:07 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+class Restaurant:
+
+    def __init__(self, restaurant_name, cuisine_tpe):
+        self.restaurant_name = restaurant_name
+        self.cuisine_type = cuisine_tpe
+
+    def describe_restaurant(self):
+        print(self.restaurant_name)
+        print(self.cuisine_type)
+
+    def open_restaurant(self):
+        print(f'\nThe {self.restaurant_name} is open')
+
+
+def main():
+    restaurant = Restaurant("Steak, Eggs, & Bitches", "Steak & Eggs")
+
+    print(restaurant.describe_restaurant())
+    print(restaurant.open_restaurant())
+
+    restaurant.open_restaurant()
+    restaurant.describe_restaurant()
+
+
+if __name__ == '__main__':
+    main()
